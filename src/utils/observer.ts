36544816c18c8a1c7ecf7eea4b2f54faa6ed7761
Mike North <michael.l.north@gmail.com>
2018-10-02 21:30:01 -0700

    fix: initial pass at docs

--- src/utils/observer.ts
+++ src/utils/observer.ts
@@ -1,6 +1,9 @@
 import { Observer, PartialObserver } from '../types';
 import { NOOP } from './constants';
 
+/**
+ * @hidden
+ */
 export function departializeObserver<T>(pobs: PartialObserver<T>): Observer<T> {
   let po = typeof pobs === 'function' ? { next: pobs } : pobs;
   return {
